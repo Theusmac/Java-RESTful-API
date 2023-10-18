798dd9dc0bea7cdbfd4fca9f843ac7cba13828ec
Matheus <matheus.ac2903@gmail.com>
2023-10-18 12:40:49 -0300

    commit inicial

---
+++ src/test/java/projeto/dio/BootcampSantander2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSantander2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
